a341e55946dd6cc6432cc0976402f0d403aca731
DAIKI <daiki.evilone@gmail.com>
2025-01-20 14:45:55 +0900

    Preview blueprint mesh.

--- PolyMeshTools/Assets/PolyMeshTools/Scripts/Editor/PolyMeshBlueprintEditor.cs
+++ PolyMeshTools/Assets/PolyMeshTools/Scripts/Editor/PolyMeshBlueprintEditor.cs
@@ -18,5 +18,39 @@ namespace eviltwo.PolyMeshTools.Editor
                 PolyMeshAssetGenerator.GenerateMeshAsset(blueprint);
             }
         }
+
+        private GUIContent _previewTitle;
+        private Mesh _previewMesh;
+        private MeshPreview _meshPreview;
+        public void OnEnable()
+        {
+            _previewTitle = new GUIContent(target.GetType().Name);
+            _previewMesh = new Mesh();
+            _meshPreview = new MeshPreview(_previewMesh);
+        }
+
+        public void OnDisable()
+        {
+            DestroyImmediate(_previewMesh);
+            _previewMesh = null;
+            _meshPreview.Dispose();
+            _meshPreview = null;
+        }
+
+        public override bool HasPreviewGUI() => true;
+
+        public override GUIContent GetPreviewTitle() => _previewTitle;
+
+        public override void OnPreviewSettings()
+        {
+            _meshPreview.OnPreviewSettings();
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            PolyMeshBlueprint blueprint = (PolyMeshBlueprint)target;
+            PolyMeshGenerator.GenerateMesh(blueprint, _previewMesh);
+            _meshPreview.OnPreviewGUI(r, background);
+        }
     }
 }
